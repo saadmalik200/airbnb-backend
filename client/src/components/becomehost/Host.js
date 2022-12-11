d7891a40ee0bec8732c81df0dddb7bdceae2d035
Saad Malik <saad_malik200@hotmail.com>
2022-12-11 13:36:56 +0100

    editing host

--- client/src/components/becomehost/Host.js
+++ client/src/components/becomehost/Host.js
@@ -1,14 +1,12 @@
 import { useContext, useState } from "react";
-import { RiArrowDownSLine } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+
 import { Context } from "../../context/Context";
-
+import { BiBuildingHouse } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
-import { BsFillCircleFill } from "react-icons/bs";
-import { MdArrowForwardIos } from "react-icons/md";
+import HousePopup from "./HousePopup";
 
-import { TbMinusVertical } from "react-icons/tb";
-
-import { FaInfoCircle } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 
 function Host() {
   const [firstName, setFirstName] = useState("");
@@ -16,6 +14,9 @@ function Host() {
 
   console.log(firstName, lastName);
   const { state, dispatch } = useContext(Context);
+
+  const [showHomePopup, setShowHomePopup] = useState(false);
+
   const navigate = useNavigate();
   return (
     <div>
@@ -24,196 +25,274 @@ function Host() {
         <div className="py-[30px]">
           <div className="passenger-flight-details px-[20px] py-[20px] flex items-center justify-between border-[0.5px] hover:shadow-xl rounded-md">
             <div className="flex items-center gap-2">
-              <p className="fs-[22px] text-[#647286] font-[700]">
-                Flight details
+              <p className="text-[36px] text-red-500 font-[700]">
+                Become a Host
               </p>
             </div>
-            <RiArrowDownSLine />
           </div>
         </div>
-        <div className=" info-2 ">
-          <p className="info-2-p1">i</p>
-          <p className="info-2-p2">
-            Please read the <span>Clarification Text </span> for more detailed
-            information regarding the Processing of Personal Data.
-          </p>
-        </div>
-        <br />
-        <div className="passenger-d-para">
-          <img
-            className="p-d-img w-[138px] h-[21px]"
-            src="https://www.turkishairlines.com/theme/img/miles-and-smiles-new-logo.png"
-            alt="miles & smiles"
-          />
-          <p className="passenger-d-p">
-            Miles&Smiles sign in provides you to save passenger details in your
-            account and use them later. Moreover, Cash&Miles feature let you pay
-            a part of your ticket with Miles and you can continue to earn Miles
-            from your flight.
-          </p>
-          <button className="passenger-d-btn">Miles&Smiles Sign in</button>
-        </div>
-        <br />
-        <div className="adult-passenger-d ">
-          <img src="https://www.turkishairlines.com/theme/img/user-grey.png" />
-          <p>Adult passenger details</p>
+        <div className="adult-passenger-d items-center gap-4">
+          <FaUserAlt />
+          <p>Personal details</p>
         </div>
         <div>
           <form className="first-form ">
-            <div className="form-group col-md-4">
-              <label for="inputState"></label>
-              <select className="form-control">
-                <option selected disabled>
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
                   Title
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Mr. / Mrs. / Ms."
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  First Name
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your first name "
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  Last Name
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your last name "
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-between mt-[2.5rem]">
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  Address
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your Address"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  City
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your city name"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  Zip Code
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your Zip Code"
+                />
+              </div>
+            </div>
+            <div className="flex gap-[4rem] mt-[2.5rem]">
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  Email
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your email address"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="font-bold mb-[.5rem] ml-[.5rem]"
+                  for="firstname"
+                >
+                  Phone Number
+                </label>
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  type="text"
+                  className="form-control w-[30rem] rounded-full"
+                  id="firstname"
+                  placeholder="Enter your phone number"
+                />
+              </div>
+            </div>
+          </form>
+
+          <br />
+        </div>
+
+        <hr className="" />
+        <br />
+
+        <div className="adult-passenger-d items-center gap-4">
+          <BiBuildingHouse className="text-[2.5rem]" />
+          <p>House details</p>
+        </div>
+        <div>
+          <form className="first-form ">
+            <div className="flex justify-between items-center ">
+              <label
+                className="font-bold mb-[.5rem] ml-[.5rem]"
+                for="firstname"
+              >
+                Which of the following best describes your place?
+              </label>
+              <select className="w-[35rem] py-[15px] pl-[16px]   rounded-full">
+                <option selected disabled className="">
+                  Select property type:
                 </option>
-                <option>Mr</option>
-                <option>Mrs</option>
+                <option>House</option>
+                <option>Apartment</option>
+                <option>Cabin</option>
+                <option>OMG</option>
+                <option>Amazing View</option>
+                <option>Aframe</option>
+                <option>House Boat</option>
               </select>
             </div>
-            <div className="form-group col-md-4">
-              <label for="firstname"></label>
+
+            <div className="flex justify-between items-center mt-[3rem]">
+              <label
+                className="font-bold mb-[.5rem] ml-[.5rem]"
+                for="firstname"
+              >
+                What type of place will guests have?
+              </label>
+              <select className="w-[35rem] py-[15px] pl-[16px]   rounded-full">
+                <option selected disabled className="">
+                  Select type of place:
+                </option>
+                <option>An Entire Place</option>
+                <option>A Private Place</option>
+                <option>A Shared Room</option>
+              </select>
+            </div>
+
+            <div className="flex justify-between items-center mt-[3rem] relative">
+              <label
+                className="font-bold mb-[.5rem] ml-[.5rem]"
+                for="firstname"
+              >
+                Share some basics about your place?
+              </label>
+
+              <div
+                onClick={() => setShowHomePopup(!showHomePopup)}
+                className="flex justify-between w-[35rem] z-10 py-[15px] pl-[16px] bg-[#EFEFEF] leading-none  rounded-full"
+              >
+                Select House Info
+                <p>
+                  <IoIosArrowDown className="text-[1.5rem] font-[900]" />
+                </p>
+              </div>
+
+              {showHomePopup && (
+                <div className="absolute top-[5rem] z-50 right-[-3.5rem]">
+                  <HousePopup />
+                </div>
+              )}
+            </div>
+
+            <div className="flex flex-col mt-[2.5rem]">
+              <label
+                className="font-bold mb-[.5rem] ml-[.5rem]"
+                for="firstname"
+              >
+                Now let's give your house a title.
+              </label>
               <input
                 onChange={(e) => setFirstName(e.target.value)}
                 type="text"
-                className="form-control"
+                className="form-control w-full rounded-full"
                 id="firstname"
-                placeholder="First / Middle name (As shown in ID)"
-              />
-            </div>
-            <div className="form-group col-md-4">
-              <label for="surname"></label>
-              <input
-                onChange={(e) => setLastName(e.target.value)}
-                type="text"
-                className="form-control"
-                id="surname"
-                placeholder="Surname (As shown in ID)"
+                placeholder="Have fun with it. You can always change it later."
               />
             </div>
 
-            <div className="form-group">
-              <label for="date"></label>
-              <input
-                type="date"
-                className="form-control"
-                id="date"
-                placeholder="DD/MM/YYYY"
-              />
+            <div className="flex flex-col mt-[2.5rem]">
+              <label
+                className="font-bold mb-[.5rem] ml-[.5rem]"
+                for="firstname"
+              >
+                Create your description.
+              </label>
+
+              <textarea
+                className="border-2 border-black py-[10px] px-[16px] rounded-2xl"
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+              ></textarea>
             </div>
-            <div className="form-group">
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="gridCheck"
-                />
-                <label className="form-check-label" for="gridCheck">
-                  Turkish citizen
-                </label>
-              </div>
-            </div>
-            <br />
-            <div className="form-row">
-              <div className="form-group col-md-4">
-                <label for="inputState"></label>
-                <select
-                  className="form-control"
-                  placeholder="Frequent flyer program"
-                >
-                  <option selected>Turkish Airlines - Miles&Smiles</option>
-                  <option>...</option>
-                </select>
-              </div>
-            </div>
-            <div className="form-group col-md-4">
-              <label for="TK"></label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Frequent flyer number"
-              />
+
+            <div className="flex justify-between items-center mt-[3rem]">
+              <label
+                className="font-bold mb-[.5rem] ml-[.5rem]"
+                for="firstname"
+              >
+                Are you a Superhost?
+              </label>
+              <select className="w-[35rem] py-[15px] pl-[16px]   rounded-full">
+                <option selected disabled className="">
+                  Select an option:
+                </option>
+                <option>Yes</option>
+                <option>No</option>
+              </select>
             </div>
           </form>
-          <hr className="hr" />
-          <form className="second-form">
-            <p className="second-form-p1"> Contact details</p>
-            <div className="second-form-inputs">
-              <div className="mb-3 col-md-4">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  placeholder="Enter email address"
-                />
-                <div id="emailHelp" className="form-text"></div>
-              </div>
-              <div class="mb-3 col-md-4">
-                <input
-                  type="code"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="Area code"
-                />
-              </div>
-              <div class="mb-3 col-md-4">
-                <input
-                  type="phone"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="Phone number"
-                />
-              </div>
-            </div>
-            <div>
-              <span className="second-f-span fw-bold py-[10px]">
-                Get to know us
-              </span>
-              <p>
-                By selecting the following communication channel(s) in
-                accordance with the terms specified in the{" "}
-                <span className="span-blue text-primary">Privacy Notice</span> I
-                consent to be contacted via the channel(s) regarding products,
-                services and opportunities offered by Turkish Airlines, its{" "}
-                <span className="span-blue text-danger">
-                  affiliates and partners
-                </span>
-                ; and that my personal data shared with Turkish Airlines for
-                marketing and customer experience activities as described in the
-                <span className="span-blue text-primary">
-                  Personal Data Announcement
-                </span>
-                to be processed, used and transferred domestically and abroad
-                for this purpose.
-              </p>
-            </div>{" "}
-            <br />
-            <div className="second-form-radio-btn flex text-dark fw-bold">
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" for="exampleCheck1">
-                  I allow notification via SMS
-                </label>
-              </div>
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" for="exampleCheck1">
-                  I allow notification via email
-                </label>
-              </div>
-            </div>
-          </form>
-          <hr className="hr" />
+
           <br />
         </div>
-        <br /> <br />
       </div>
 
       {/* //////////////////////////////////////////////////// */}
